1b34cae02920acc7893eaf5dd1674f7a72baa27d
alexey <aarylov@gmail.com>
2022-07-10 18:08:26 +0500

    max of two numbers

---
+++ HomeWork01/EX02/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.Write("max = " + num1);
+} else if (num2 > num1)
+{
+    Console.Write("max = " + num2);
+} else
+{
+    Console.Write("числа равны");
+}
