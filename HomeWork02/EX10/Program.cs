bb6979f05f8299ccfc2eb1bb6a5516cac18430a2
alexey <aarylov@gmail.com>
2022-07-14 17:13:43 +0500

    second number of three-digit number

---
+++ HomeWork02/EX10/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите трехзначное целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (((number / 100) < 1) | ((number / 1000) >= 1))
+{
+    Console.WriteLine("Число не трехзначное!");
+}
+else
+{
+    int result = Cut(number);
+    Console.WriteLine("Вторая цифра: " + result);
+}
+
+int Cut(int a)
+{
+    int b = a / 10;
+    int c = b % 10;
+    return c;
+}
