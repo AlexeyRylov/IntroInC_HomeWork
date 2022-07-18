2b6535674b6038647507e3d705626dafe5561eb0
alexey <aarylov@gmail.com>
2022-07-18 10:53:56 +0500

    cube table from 1 to entered number

---
+++ HomeWork03/EX23/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите число: ");               //Организуем ввод
+int numberIn = int.Parse(Console.ReadLine());
+
+Cube(numberIn);
+
+void Cube(int numberFunc)
+{
+    int i = 1;
+    if (numberFunc > 1)                         //Расчет при положительном введенном числе
+    {
+        while (i <= numberFunc)
+        {
+            Console.Write(i * i * i + " ");
+            i++;
+        }
+    }
+    else                                        //Расчет при отрицательном введенном числе
+    {
+        while (i >= numberFunc)
+        {
+            Console.Write(i * i * i + " ");
+            i--;
+        }
+    }
+}
