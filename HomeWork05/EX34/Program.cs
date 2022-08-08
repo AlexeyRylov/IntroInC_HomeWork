6e896afc468499f922ca7e411ddde8bebe69514c
alexey <aarylov@gmail.com>
2022-08-08 22:10:37 +0300

    number of even numbers

---
+++ HomeWork05/EX34/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+Console.Write("Введите размерность массива: ");
+int range = int.Parse(Console.ReadLine());
+int[] array = new int[range];
+
+Random rand = new Random();
+int result = 0;
+
+for (int i = 0; i < range; i++)
+{
+    array[i] = rand.Next(100, 1000);
+    Console.Write(array[i] + " ");
+    if (array[i] % 2 == 0)
+    {
+        result += 1;
+    }
+}
+
+Console.WriteLine();
+Console.Write("Количество четных чисел в массиве равно " + result);
