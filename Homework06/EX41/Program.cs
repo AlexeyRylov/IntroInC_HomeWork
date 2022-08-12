e31ee70564e14735e6491bcdb228fb832d7b1e3e
alexey <aarylov@gmail.com>
2022-08-12 21:40:27 +0300

    check positive numbers

---
+++ Homework06/EX41/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4
+*/
+int CalcPositiveNumbers(string[] funcArray)
+{
+    int[] tmpArray = new int[funcArray.Length];
+    int count = 0;
+    for (int i = 0; i < funcArray.Length; i++)
+    {
+        tmpArray[i] = int.Parse(funcArray[i]);
+        if (tmpArray[i] > 0)
+        {
+            count +=1;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите числа, разделите их пробелом: ");
+string initString = Console.ReadLine();
+string[] initSplit = initString.Split(' ');
+
+int result = CalcPositiveNumbers(initSplit);
+Console.WriteLine("Количество положительных чисел: " + result);
+
+
+
