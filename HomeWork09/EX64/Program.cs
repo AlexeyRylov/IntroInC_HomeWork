a8bb2569d8cf2a9fcdd68c22561d215c8b71ccd6
alexey <aarylov@gmail.com>
2022-08-25 09:18:46 +0500

    PrintNaturalNumbers

---
+++ HomeWork09/EX64/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+
+void PrintNaturalNumbers(int m, int n)
+{
+    if (m > n) { return; }
+    Console.Write(m + "\t");
+    m++;
+    PrintNaturalNumbers(m, n);
+}
+
+
+Console.WriteLine("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+PrintNaturalNumbers(m, n);
