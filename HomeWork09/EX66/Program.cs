f6c5d3c7f1e6c9e54f8ca31acb67e7d18edd9bab
alexey <aarylov@gmail.com>
2022-08-25 09:19:23 +0500

    SumNaturalNumbers

---
+++ HomeWork09/EX66/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNaturalNumbers(int m, int n, int sum)
+{
+    if (m > n) { return sum; }
+    sum += m;
+    m++;
+    return SumNaturalNumbers(m, n, sum);
+}
+
+
+Console.WriteLine("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+Console.WriteLine(SumNaturalNumbers(m, n, sum));
